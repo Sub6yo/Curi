4a01becca0b2032d5459092795b1e20774958654
Sub6yo <145216567+Sub6yo@users.noreply.github.com>
2024-05-31 01:18:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 # (©)Codexbotz
-# Recode by @mrismanaziz
-# t.me/SharingUserbot & t.me/Lunatic0de
+
+
+
 
 import logging
 import os
-from distutils.util import strtobool
-from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
-load_dotenv("config.env")
+
 
 # Bot token dari @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
