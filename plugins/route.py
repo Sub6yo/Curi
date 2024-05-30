98739027100bc0d86e305eb690a4f8f2566f865e
Sub6yo <145216567+Sub6yo@users.noreply.github.com>
2024-05-31 00:55:04 +0700

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,8 +1,8 @@
-#rymme
+rymme
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("IndomieGorengSatu")
+    return web.json_response("CodeXBotz")
